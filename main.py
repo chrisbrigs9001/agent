75e03ee1e697e7dc81eb8c7f50d89d5b1d5e232c
chrisbrigs9001 <chrisbrigs9001@gmail.com>
2026-01-14 10:18:44 -0600

    token checks

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv("sus.env")
+load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 
@@ -14,6 +14,13 @@ def main():
 
     prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     output = client.models.generate_content(model="gemini-2.5-flash", contents=prompt)
+    
+    if(output.usage_metadata==None):
+        raise RuntimeError("Metadata is none, failed API request")
+    print(f"User prompt: {prompt}")
+    print(f"Prompt tokens: {output.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {output.usage_metadata.candidates_token_count}")
+    
     print(output.text)
 
 if __name__ == "__main__":
