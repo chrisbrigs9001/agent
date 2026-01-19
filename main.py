fb9caec343186d9331d8544dbbbddca1f0e4eeb6
chrisbrigs9001 <chrisbrigs9001@gmail.com>
2026-01-18 21:39:50 -0600

    prompt loop

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import os
+import os, sys
 import argparse
 
-from call_function import available_functions
+from call_function import available_functions, call_function
 from prompts import system_prompt
 
 from google import genai
@@ -29,7 +29,22 @@ def main():
     #create new list of types.Content, set the users prompt as the message
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     
+    if(args.verbose):
+        print(f"User prompt: {args.user_prompt}")
+    
     #prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    for i in range (20):
+        f = gen_content(client, messages, args.verbose)
+        if f:
+            return
+    print("Max iterations reached")
+    sys.exit(1)
+        
+        
+    
+
+
+def gen_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.5-flash", 
         contents=messages,
@@ -37,23 +52,43 @@ def main():
             tools=[available_functions],
             system_instruction=system_prompt),
     )
+    if response.candidates:
+        for c in response.candidates:
+            messages.append(c.content)
     
     if(not response.usage_metadata):
         raise RuntimeError("Metadata is none, failed API request")
     
-    if(args.verbose):
-        print(f"User prompt: {args.user_prompt}")
+    if verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    
     
     if (not response.function_calls):
         print("Response:")
         print(response.text)
-        return
-    for function_call in response.function_calls:
-        print(f"Calling function: {function_call.name}({function_call.args})")
+        return True
     
+    function_results_list = []
+    
+    for function_call in response.function_calls:
+        #print(f"Calling function: {function_call.name}({function_call.args})")
+        function_call_result = call_function(function_call, verbose)
+        if not function_call_result.parts:
+            raise Exception ("Empty parts")
+        if not function_call_result.parts[0].function_response:
+            raise Exception ("None parts")
+        if not function_call_result.parts[0].function_response.response:
+            raise Exception ("None response")
+        function_results_list.append(function_call_result.parts[0])
+        
+        
+        if (verbose):
+            print(f"-> {function_call_result.parts[0].function_response.response['result']}")
+    messages.append(types.Content(role="user", parts=function_results_list))
+    
+    return False        
+    
+
 
 if __name__ == "__main__":
     main()
--- prompts.py
+++ prompts.py
@@ -9,4 +9,13 @@ When a user asks a question or makes a request, make a function call plan. You c
 - Write or overwrite files
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+
+If addressing the user, always respond to the user as one of the following titles:
+    The Glizzler
+    Glizzy Gobbler
+    Glizzinator
+    Glorper of Glizzies
+    Glizz Goblin
+    
+Preface your final responses with 'Ah, my lord the [insert title from the above list here]! My final response to your question is:'
 """
